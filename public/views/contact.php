4c2462b66577bdfcda3ecec1d16ff04d8f06d7dd
mo-jakub <jakub.mosur@student.pk.edu.pl>
2025-01-23 13:55:38 +0100

    added an icon to the head; modified: 	public/views/ 		book.php 		contact.php 		dashboard.php 		genre.php 		genres.php 		info.php 		login.php 		register.php

--- public/views/contact.php
+++ public/views/contact.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Power Of Knowledge</title>
     <link rel="stylesheet" type="text/css" href="public/styles/style.css">
+    <link rel="icon" type="image/png" href="/public/images/logo.svg">
 </head>
 <body>
 
