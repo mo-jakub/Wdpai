66ec1f9041ba09e66a3926a0172b0df20ea43615
mo-jakub <jakub.mosur@student.pk.edu.pl>
2025-01-25 04:42:11 +0100

    changed styling and added a dropdown menu in view for mobile; new file: public/scripts/menu.js modified: 	public/partials/header.php 	public/styles/style.css

--- public/partials/header.php
+++ public/partials/header.php
@@ -1,42 +1,88 @@
+<script src="/public/scripts/menu.js"></script>
 <header>
     <div class="container">
-        <a href="/" class="logo-link">
-            <img src="/public/images/logo.svg" alt="Logo" class="logo">
-            Power of Knowledge
-        </a>
-        <nav class="nav-wrapper">
-            <a href="/" class="nav-link">
-                <img src="/public/images/header/home.svg" alt="" class="logo">
-                Home
+        <div class="mobile-header container">
+            <a href="/" class="logo-link">
+                <img src="/public/images/logo.svg" alt="Logo" class="logo">
+                Power of Knowledge
             </a>
-            <a href="/genres" class="nav-link">
-                <img src="/public/images/header/box.svg" alt="" class="logo">
-                Genres
-            </a>
-            <a href="/tags" class="nav-link">
-                <img src="/public/images/header/tag.svg" alt="" class="logo">
-                Tags
-            </a>
-        </nav>
-        <div class="search-bar">
-            <input type="text" placeholder="Search by title or author">
+            <div class="mobile-nav">
+                <div class="menu-icon">
+                    <img src="/public/images/header/menu.svg" alt="Menu">
+                </div>
+                <div class="dropdown-menu">
+                    <div class="search-bar">
+                        <input type="text" placeholder="Search by title or author">
+                    </div>
+                    <a href="/" class="nav-link">
+                        <img src="/public/images/header/home.svg" alt="" class="logo">
+                        Home
+                    </a>
+                    <a href="/genres" class="nav-link">
+                        <img src="/public/images/header/box.svg" alt="" class="logo">
+                        Genres
+                    </a>
+                    <a href="/tags" class="nav-link">
+                        <img src="/public/images/header/tag.svg" alt="" class="logo">
+                        Tags
+                    </a>
+                    <?php if (isset($_COOKIE['session_token'])): ?>
+                        <a href="/logout" class="nav-link">
+                            <img src="/public/images/header/logout.svg" alt="" class="logo">
+                            Logout
+                        </a>
+                    <?php else: ?>
+                        <a href="/login" class="nav-link">
+                            <img src="/public/images/header/login.svg" alt="" class="logo">
+                            Login
+                        </a>
+                        <a href="/register" class="nav-link">
+                            <img src="/public/images/header/signup.svg" alt="" class="logo">
+                            Sign Up
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
         </div>
-        <nav class="nav-wrapper auth">
-            <?php if (isset($_COOKIE['session_token'])): ?>
-                <a href="/logout" class="nav-link">
-                    <img src="/public/images/header/logout.svg" alt="" class="logo">
-                    Logout
+        <div class="pc-header container">
+            <a href="/" class="logo-link">
+                <img src="/public/images/logo.svg" alt="Logo" class="logo">
+                Power of Knowledge
+            </a>
+            <nav class="nav-wrapper">
+                <a href="/" class="nav-link">
+                    <img src="/public/images/header/home.svg" alt="" class="logo">
+                    Home
                 </a>
-            <?php else: ?>
-                <a href="/login" class="nav-link">
-                    <img src="/public/images/header/login.svg" alt="" class="logo">
-                    Login
+                <a href="/genres" class="nav-link">
+                    <img src="/public/images/header/box.svg" alt="" class="logo">
+                    Genres
                 </a>
-                <a href="/register" class="nav-link">
-                    <img src="/public/images/header/signup.svg" alt="" class="logo">
-                    Sign Up
+                <a href="/tags" class="nav-link">
+                    <img src="/public/images/header/tag.svg" alt="" class="logo">
+                    Tags
                 </a>
-            <?php endif; ?>
-        </nav>
+            </nav>
+            <div class="search-bar">
+                <input type="text" placeholder="Search by title or author">
+            </div>
+            <nav class="nav-wrapper auth">
+                <?php if (isset($_COOKIE['session_token'])): ?>
+                    <a href="/logout" class="nav-link">
+                        <img src="/public/images/header/logout.svg" alt="" class="logo">
+                        Logout
+                    </a>
+                <?php else: ?>
+                    <a href="/login" class="nav-link">
+                        <img src="/public/images/header/login.svg" alt="" class="logo">
+                        Login
+                    </a>
+                    <a href="/register" class="nav-link">
+                        <img src="/public/images/header/signup.svg" alt="" class="logo">
+                        Sign Up
+                    </a>
+                <?php endif; ?>
+            </nav>
+        </div>
     </div>
 </header>
---
+++ public/scripts/menu.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+
+    menuIcon.addEventListener("click", function () {
+        dropdownMenu.classList.toggle("active");
+    });
+
+    // Zamknij menu, gdy kliknięto poza nim
+    document.addEventListener("click", function (e) {
+        if (!menuIcon.contains(e.target) && !dropdownMenu.contains(e.target)) {
+            dropdownMenu.classList.remove("active");
+        }
+    });
+});
