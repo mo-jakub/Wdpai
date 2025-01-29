1c5a6e35952f108cb06f7e0e90de0ff77a99ce16
mo-jakub <jakub.mosur@student.pk.edu.pl>
2025-01-30 00:16:47 +0100

    added listener so that the script starts to work only when the page is loaded

--- public/scripts/sessionRenew.js
+++ public/scripts/sessionRenew.js
@@ -1,15 +1,17 @@
-let debounceTimeout;
+document.addEventListener('DOMContentLoaded', () => {
+    let debounceTimeout;
 
-function extendSession() {
-    clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() => {
-        fetch('/extendSession', { method: 'POST' })
-            .then(response => response.json())
-            .then(data => console.log('Session extended:', data))
-            .catch(error => console.error('Error renewing session:', error));
-    }, 900000); // 15-min delay
-}
+    function extendSession() {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => {
+            fetch('/extendSession', { method: 'POST' })
+                .then(response => response.json())
+                .then(data => console.log('Session extended:', data))
+                .catch(error => console.error('Error renewing session:', error));
+        }, 900000); // 15-min delay
+    }
 
-document.body.addEventListener('mousemove', extendSession);
-document.body.addEventListener('keydown', extendSession);
-document.body.addEventListener('click', extendSession);
+    document.body.addEventListener('mousemove', extendSession);
+    document.body.addEventListener('keydown', extendSession);
+    document.body.addEventListener('click', extendSession);
+});
