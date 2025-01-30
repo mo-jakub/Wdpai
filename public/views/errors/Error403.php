e443d0f79b7d1d2f1afc728c7c71aeeabf33c9a3
mo-jakub <jakub.mosur@student.pk.edu.pl>
2025-01-30 03:28:43 +0100

    added error 403 page

---
+++ public/views/errors/Error403.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Power Of Knowledge</title>
+    <link rel="stylesheet" type="text/css" href="/public/styles/style.css">
+</head>
+<body>
+
+<?php include __DIR__ . '/../parts/header.php'; ?>
+
+<main class="page">
+    <div class="info">
+        <img src="/public/images/on-page-logo.svg" alt="Logo" class="on-page-logo">
+        <h3>
+            <p>Error 403</p>
+            <p>You're forbidden from accessing this site.</p>
+        </h3>
+    </div>
+</main>
+
+<?php include __DIR__ . '/../parts/footer.php'; ?>
+
+</body>
+</html>
